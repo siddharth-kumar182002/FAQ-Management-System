2fdd30ae78caa694c9e56992b6ace2011ad83b52
siddharth-kumar182002 <sahilkumar182002@gmail.com>
2025-02-02 19:36:59 +0530

    Added Deployed backend Proxy

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
      proxy:{
-        '/api':'http://localhost:8000',
+        '/api':'https://faq-management-system-backend.onrender.com',
      },
   },
   plugins: [react()],
